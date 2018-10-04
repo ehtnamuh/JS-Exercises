b1e0dff5bd043c582efd85878ef49342dca44a5a
Samin Bin Karim <saminbinkarim@gmail.com>
2018-10-04 03:18:39 +0000

    Task 2

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 /* JavaScript code here */
-document.write("Cyka Blyat idi Nahui");
+//document.getElementById("demo").innerHTML = 5 + 6;
+console.log("EXTERNAL SCRIPt");
+
+
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value, index){
+        sum = sum+value;
+    });
+    return sum;
+}
+
+var Sarr = [1, 2, 3, 5,  4, 10];
+
+console.log(sumArray(Sarr));
+
